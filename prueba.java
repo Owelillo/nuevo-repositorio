b40e3781acbc48d8022268610dd67584769df700
Gabriel <owelcontacto@gmail.com>
2023-10-16 09:25:42 +0200

    Ahora tambien indica mi comida favorita

--- prueba.java
+++ prueba.java
@@ -5,6 +5,7 @@ public class prueba {
 		System.out.println("Rojas Barredo");
 		System.out.println("Edad: 18");
 		System.out.println("Fecha de nacimiento: 2005");
+		System.out.println("Comida favorita: Solomillo empanado marinado");
 	}
 }
 
