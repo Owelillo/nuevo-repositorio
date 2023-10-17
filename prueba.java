32631f87a8b0839ffb265e84e2344be0cbf0ba81
Gabriel <owelcontacto@gmail.com>
2023-10-17 13:02:06 +0200

    Elimino la primera linea y añado mi cantante favorito al final

--- prueba.java
+++ prueba.java
@@ -2,10 +2,10 @@ public class prueba {
 
 	public static void main(String args[]){
 
-		System.out.println("Rojas Barredo");
 		System.out.println("Edad: 18");
 		System.out.println("Fecha de nacimiento: 2005");
 		System.out.println("Comida favorita: Solomillo empanado marinado");
+		System.out.println("Cantante favorito: Nerwi");
 	}
 }
 
